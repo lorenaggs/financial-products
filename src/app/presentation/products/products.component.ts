553b5ceaa0bf0347ae73b74e7b30a5681cb6cb44
lguartazaca <lguartazaca@opcionauto.com>
2025-03-11 21:42:17 -0500

    feat(api): enhance financial product retrieval with error handling

---
+++ src/app/presentation/products/products.component.ts
@@ -0,0 +1,42 @@
+import {Component, OnInit} from '@angular/core';
+import {FinancialProductApiService} from '../../infrastructure/adapters/serviceCommon';
+import {CommonModule} from '@angular/common';
+
+@Component({
+  selector: 'app-products',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './products.component.html',
+  styleUrl: './products.component.scss'
+})
+export class ProductsComponent implements OnInit {
+
+  listProducts: any[] = [];
+
+  constructor(
+    public financialProductService: FinancialProductApiService,
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.financialProductService.getFinancialProducts().subscribe(
+      (products) => {
+        this.listProducts = products;
+        console.log(products);
+      }
+    );
+  }
+
+  addProduct(): void {
+    /*this.financialProductService.createFinancialProduct({
+      id: '1',
+      name: 'Producto 1',
+      description: 'Descripcion del producto 1',
+      price: 1000
+    }).subscribe(
+      (response) => {
+        console.log(response);
+      }
+    );*/
+  }
+}
